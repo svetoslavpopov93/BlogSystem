291ae4719c0ea3cb64ef6a92f2989eeb20ee24f7
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-01-16 00:50:04 +0200

    Sceleton added

---
+++ BlogSystem/BlogSystem/js/app.js
@@ -0,0 +1,22 @@
+﻿(function () {
+	var app= angular.module('app', ["ngRoute"]);
+	app.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+        .when('/home',
+        {
+        	controller: 'HomeController',
+        	templateUrl: '/views/home-view.html'
+        })
+        .when('/posts',
+        {
+        	controller: 'PostsController',
+        	templateUrl: '/views/posts-view.html'
+        })
+        .otherwise({ redirectTo: '/home' });
+	}]);
+	// configure existing services inside initialization blocks.
+	app.config(['$locationProvider', function ($locationProvider) {
+		// Configure existing providers
+		$locationProvider.hashPrefix('');
+	}]);
+}());
